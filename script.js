6dcd6ec7e82680e39bf3ccdaf443073f5b08379c
Pavel Gritsuk <pavlik.morozov701@gmail.com>
2021-06-16 11:51:47 +0300

    finished

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const request = new XMLHttpRequest();
 const url = "http://localhost:3002";
 const modalRegisterForm = document.getElementById('modal-registr-form');
 
-class SendData{
+class SendData{ 
     id; 
-    name;
-    surname;
-    age;
-}
+    name; 
+    surname; 
+    age;}
 
 
 //Get request
@@ -27,19 +26,15 @@ request.send();
 
 
 //Post request
-
-const registrUser = document.getElementById('registrUser');
-
-function sendPostRequest(event){
+function sendPostRequest(event, registrFormInputs){
 
     event.preventDefault();
 
 
     let requestBody = new SendData
-    const modalFormInputs = modalRegisterForm.getElementsByTagName('input')
 
-    for(let i = 0; i < modalFormInputs.length; i++){
-        requestBody[modalFormInputs[i].name] = modalFormInputs[i].value
+    for(let i = 0; i < registrFormInputs.length; i++){
+        requestBody[registrFormInputs[i].name] = registrFormInputs[i].value
     };
 
     console.log(requestBody);
@@ -49,129 +44,61 @@ function sendPostRequest(event){
     request.setRequestHeader("Content-type", "application/json");
     request.onreadystatechange = function() {
         if(request.readyState == XMLHttpRequest.DONE && (request.status == 200 || request.status == 201)) {
-            console.log('OK');
+            alert(JSON.parse(request.responseText).answer);
         };
     }
     
     request.send(JSON.stringify(requestBody));
     
-}
-
-registrUser.onclick = function(event){
-    sendPostRequest(event);
-}
+}   
 
 
 //PATCH request
 
-function sendPatchRequest(event){
+function sendPatchRequest(event, editFormInputs){
 
     event.preventDefault();
 
-    const modalFormInputs = modalForm.getElementsByTagName('input')
-
-    for(let i = 0; i < modalFormInputs.length; i++){
-        newData[modalFormInputs[i].name] = modalFormInputs[i].value
+    for(let i = 0; i < editFormInputs.length; i++){
+        newData[editFormInputs[i].name] = editFormInputs[i].value
     };
     
     request.open('PATCH', url + '/update', true);
 
     request.setRequestHeader("Content-type", "application/json");
     request.onreadystatechange = function() {
-        if(request.readyState == XMLHttpRequest.DONE && (request.status == 200)) {
-            console.log(request.status);
+        if(request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            alert(JSON.parse(request.responseText).answer);
+            console.log(JSON.parse(request.responseText).answer);
         };
     }
     
     requestBody = {oldData, newData};
 
     request.send(JSON.stringify(requestBody));
-  
 }
 
 
 //Delete request
 
-function deleteUserRequest(user_id){
-    
+function deleteUserRequest(event, user_id){
+
+    event.preventDefault();
     
     request.open('DELETE', url + '/delete?id=' + user_id);
 
     request.setRequestHeader('Content-Type', 'application/x-www-form-url');
 
-    request.send(null);
-
     request.addEventListener("readystatechange", () =>{
         if(request.readyState === 4 && request.status == 200 ){
-            console.log('Delete');  
+            alert(JSON.parse(request.responseText).answer);  
+            console.log(JSON.parse(request.responseText).answer);
         }
     })
+
+    request.send(null);
     
 }
-
-
-//Register Modal Form
-const registerNav = document.getElementById('registration');
-
-function showCover() {
-    
-    let coverDiv = document.createElement('div');
-    coverDiv.id = 'cover-div';
-
-    document.body.style.overflowY = 'hidden';
-
-    document.body.append(coverDiv);
-}
-
-
-function hideCover() {
-    document.getElementById('cover-div').remove();
-    document.body.style.overflowY = '';
-  }
-
-
-function showModalRegistrationForm(){
-    
-    showCover();
-    let modalFormContainer = document.getElementById('modal-registrform-block')
-    modalFormContainer.style.display = 'block';
-
-
-    function complete(value) {
-        hideCover();
-        modalFormContainer.style.display = 'none';
-        document.onkeydown = null;
-    }
-    
-    /*
-    modalRegisterForm.onsubmit = function() {
-        complete(value);
-        return false;
-    };
-    */
-
-    modalRegisterForm.cancel.onclick = function() {
-        complete(null);
-    };
-    
-
-    document.onkeydown = function(e) {
-        if (e.key == 'Escape') {
-            complete(null);
-        }
-    };
-    
-    // Hide cover
-    const overModalClick = document.getElementById('cover-div');
-
-    overModalClick.addEventListener('click', complete);
-
-
-}
-
-registerNav.addEventListener('click', showModalRegistrationForm);
-
-///////////////////////////////////////////////////////////
 
 
 //Rendering JSON Data
@@ -213,89 +140,238 @@ function renderAllUser(usersArray){
 
 };
 
-///////////////////////////////////////////////////////////
 
-//Edit and delete User Form
+//Register Modal Form
+const registerNav = document.getElementById('registration');
+const saveRegistrUser = document.getElementById('registrUser');
+const registerFormContainer = document.getElementById('modal-registrform-block')
+const registrFormInputs = modalRegisterForm.getElementsByTagName('input')
 
-const editUserBtn = document.getElementById('editUser');
-const deleteUserBtn = document.getElementById('deleteUser');
-const modalForm = document.getElementById('modal-edit-form');
+
+function clearInputValue(formInputs){
+    for(let i = 0; i < formInputs.length; i++){
+        formInputs[i].value = '';
+    }
+}
+
+function showCover() {
+    
+    let coverDiv = document.createElement('div');
+    coverDiv.id = 'cover-div';
+
+    document.body.style.overflowY = 'hidden';
+
+    document.body.append(coverDiv);
+}
+
+
+function hideCover() {
+    document.getElementById('cover-div').remove();
+    document.body.style.overflowY = '';
+  }
+
+
+function complete(value, formContainer) {
+    hideCover();
+    formContainer.style.display = 'none';
+    document.onkeydown = null;
+}
+
+function closeModal(formContainer, form){
+    
+    
+    form.cancel.onclick = function() {
+        complete(null, formContainer);
+    };
+    
+    document.onkeydown = function(e) {
+        if (e.key == 'Escape') {
+            complete(null, formContainer);
+        }
+    };
+
+    const overModalClick = document.getElementById('cover-div');
+
+    overModalClick.addEventListener('click', function(){
+        complete(null, formContainer);
+    }); 
+
+}
+
+
+function showRegisterForm(registerFormContainer){
+
+    const registrForm = registerFormContainer.getElementsByTagName('form')[0];
+
+    showCover();
+    clearInputValue(registrFormInputs);
+    registerFormContainer.style.display = 'block';
+
+
+    //Send request and close modal
+    saveRegistrUser.onclick = function(event){
+        validateData(event, registrForm)
+        let errors = registrForm.querySelectorAll('.error');
+        if(errors.length == 0){
+            sendPostRequest(event, registrFormInputs);
+            complete(null, registerFormContainer)
+        }
+    }
+
+    
+    closeModal(registerFormContainer, registrForm);
+}
+
+registerNav.addEventListener('click', function(){
+    showRegisterForm(registerFormContainer)
+});
+
+
+//Edit and delete modal form
+
+const saveEditUser = document.getElementById('editUser');
+const deleteUser = document.getElementById('deleteUser');
+const editFormContainer = document.getElementById('modal-editform-block');
 const oldData = new SendData    
 const newData = new SendData
 
 
 
-function showModalEditForm(userData){
+function showModalEditForm(userData, editFormContainer){
 
     showCover();
-    let modalFormContainer = document.getElementById('modal-editform-block')
-    let modalFormInputs = modalForm.getElementsByTagName('input')
-    let data = userData.getElementsByTagName('td');
-
+    const editForm = editFormContainer.getElementsByTagName('form')[0];
+    const editFormInputs = editForm.querySelectorAll('input');
+    const data = userData.getElementsByTagName('td');
 
     for(let i = 0; i < data.length - 1; i++){
-        modalFormInputs[i].value = data[i].innerHTML;
-        oldData[modalFormInputs[i].name] = data[i].innerHTML;
+        editFormInputs[i].value = data[i].innerHTML;
+        oldData[editFormInputs[i].name] = data[i].innerHTML;
     }
 
+    editFormContainer.style.display = 'block';
 
-    function complete(value) {
-        hideCover();
-        modalFormContainer.style.display = 'none';
-        document.onkeydown = null;
-    }
-    
-    /*
-    form.onsubmit = function() {
-        complete(value);
-        return false;
-    };
-    */
-
-    modalForm.cancel.onclick = function() {
-        complete(null);
-    };
-    
-
-    document.onkeydown = function(e) {
-        if (e.key == 'Escape') {
-            complete(null);
-        }
-    };
-
-    modalFormContainer.style.display = 'block';
-
-    //Edit User
-    editUserBtn.onclick = function(event){
-        sendPatchRequest(event);
-    }
 
     //Delete User
-    deleteUserBtn.onclick = function(event){
-        event.preventDefault()
-        deleteUserRequest(oldData.id);
+    deleteUser.onclick = function(event){
+        deleteUserRequest(event, oldData.id);
+        complete(null, editFormContainer)
     }
 
-    // Hide cover
-    const overModalClick = document.getElementById('cover-div');
+    //Save Edit and close modal
+    saveEditUser.onclick = function(event){
+        validateData(event, editForm)
+        let errors = editForm.querySelectorAll('.error');
+        if(errors.length == 0){
+            sendPatchRequest(event, editFormInputs);
+            complete(null, editFormContainer)
+        }
+    }
 
-    overModalClick.addEventListener('click', complete);
+    editForm.onsubmit = function(event){
+        validateData(event, editForm)
+    }
+    
+    closeModal(editFormContainer, editForm)
 
 }
 
 
-
-function editUsers(){
+function editUsers(editFormContainer){
     const editButtons = document.getElementsByClassName("edit-button");
     for(let i = 0; i < editButtons.length; i++){
         editButtons[i].addEventListener('click', function(){
             userDataStream = editButtons[i].parentNode.parentNode;
-            showModalEditForm(userDataStream);
+            showModalEditForm(userDataStream, editFormContainer);
         })
     }
 }
 
 
 window.onload = function(){ // Исправвить необх постоянной перезагрузки
-    editUsers();
+    editUsers(editFormContainer);
 }
+
+
+// Validator
+
+function validateData(event, form){
+
+    
+    function createErrorDiv(error, formInput){
+        if(error != undefined){
+            const errorDiv = document.createElement('div');
+            errorDiv.className = 'error';
+            errorDiv.style.color = 'red';
+            errorDiv.innerHTML = error;;
+            formInput.parentElement.insertBefore(errorDiv, formInput);        
+        }
+    
+    }
+
+
+    function number_field_error_handler(input){
+    
+        let error;
+    
+        if(input.value <=  0){
+            error = 'This field can not be 0 or less.' 
+        }else if(input.name == 'age' && input.value > 100){
+            error = 'Incorrect age value.'
+        }
+    
+    }
+    
+    function text_field_error_handler(input){
+        
+        let error;
+    
+        if(/[0-9]/.test(input.value)){
+            error = 'Field ' + input.name + ' shouldn\'t contain numbers.'
+        } else if(input.value.length < 5){
+            error = 'Field ' + input.name + ' should be longer than 5 symbol.'
+        } else if(input.value.length > 50){
+            error = 'Field ' + input.name + ' shouldn\'t be longer than 50 symbol.'
+        }
+    
+        createErrorDiv(error, input);
+    }
+
+
+    function empty_error_handler(event, form){
+    
+        const formInputs = form.getElementsByTagName('input');
+
+        for(let i = 0; i < formInputs.length; i++){
+            
+            if(formInputs[i].value == ''){
+                
+                createErrorDiv('Is empty', formInputs[i]);
+                
+            } else {
+    
+                if (formInputs[i].name == 'id' || formInputs[i].name == 'age'){
+                    
+                    number_field_error_handler(formInputs[i]);
+                }else{
+    
+                    text_field_error_handler(formInputs[i]);
+    
+                }
+            }
+        }
+
+        event.preventDefault();
+    }
+
+
+    let errors = document.querySelectorAll('.error');
+
+    for(let i = 0; i < errors.length; i++){
+        errors[i].remove();
+    }
+
+    empty_error_handler(event, form);    
+
+}
+
